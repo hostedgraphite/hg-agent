a3636fc933e0cfdc496c003a441cd749d0488586
finnci <4cfinn@gmail.com>
2017-07-13 11:09:19 +0100

    import correctly, build builds, package tests

--- build.sh
+++ build.sh
@@ -23,7 +23,8 @@ pip install pyinstaller==3.1.1    \
             supervisor==3.3.1     \
             diamond==4.0.451      \
             psutil==5.1.3         \
-            'git+ssh://git@github.com/hostedgraphite/hg-agent-periodic.git@96b389f882e8958929dea146c49b71aed0bbffac#egg=hg_agent_periodic'
+            multitail2==1.4.1            \
+            'git+ssh://git@github.com/hostedgraphite/hg-agent-periodic.git@96b389f882e8958929dea146c49b71aed0bbffac#egg=hg_agent_periodic'\
             'git+ssh://git@github.com/metricfire/hg-agent-forwarder.git@update/receiver-forwarder#egg=hg_agent_forwarder'
 # Workaround a PyInstaller issue with namespaced packages, cf. goo.gl/CnuoMo
 touch /hg-agent.venv/lib/python2.7/site-packages/supervisor/__init__.py
