bf210d1156cc82b318b023493fe47eecdc9edf51
finnci <4cfinn@gmail.com>
2017-07-20 16:33:47 +0100

    add git hash for specific hg-agent-forwrader

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ pip install pyinstaller==3.1.1    \
             psutil==5.1.3         \
             multitail2==1.4.1            \
             'git+ssh://git@github.com/hostedgraphite/hg-agent-periodic.git@96b389f882e8958929dea146c49b71aed0bbffac#egg=hg_agent_periodic'\
-            'git+ssh://git@github.com/hostedgraphite/hg-agent-forwarder.git@update/receiver-forwarder#egg=hg_agent_forwarder'
+            'git+ssh://git@github.com/hostedgraphite/hg-agent-forwarder.git@4f21ed807ed89037f972db7d13845d147975003e#egg=hg_agent_forwarder'
 # Workaround a PyInstaller issue with namespaced packages, cf. goo.gl/CnuoMo
 touch /hg-agent.venv/lib/python2.7/site-packages/supervisor/__init__.py
 
